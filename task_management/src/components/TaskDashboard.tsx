c2e56e17c595c2a7cad5ddc190d8fab75070e7a1
b-smith94 <byrondsmith9@gmail.com>
2025-01-12 18:07:44 -0500

    added toggle button to toggle between showing and hiding task details

--- task_management/src/components/TaskDashboard.tsx
+++ task_management/src/components/TaskDashboard.tsx
@@ -1,10 +1,12 @@
 import { Button, ListGroup } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useTasksContext } from "../TaskContext";
+import { useState } from "react";
 
 const TaskDashboard: React.FC = () => {
     const { tasks, dispatch } = useTasksContext();
     const navigate = useNavigate();
+    const [expandedTaskId, setExpandedTaskId] = useState<number | null>(null);
 
     const handleRemoveTask = (taskId: number) => {
         dispatch({ type: 'DELETE_TASK', payload: taskId });
@@ -13,6 +15,11 @@ const TaskDashboard: React.FC = () => {
     const handleCompleteTask = (taskId: number) => {
         dispatch({ type: 'COMPLETE_TASK', payload: taskId})
     }
+
+    const toggleTaskDetails = (taskId: number) => {
+        setExpandedTaskId(expandedTaskId === taskId? null : taskId);
+    }
+
     return (
         <div>
             <h1>Task Dashboard</h1>
@@ -20,7 +27,14 @@ const TaskDashboard: React.FC = () => {
             <ListGroup>
                 {tasks && tasks.length > 0 ? tasks.map(task => (
                     <ListGroup.Item key={task.id}>
-                        <span>Task: {task.name}</span>
+                        <span><strong>Task: </strong>{task.name}</span>
+                        <Button variant="info" role="button" onClick={() => toggleTaskDetails(task.id)}>{expandedTaskId === task.id ? 'Hide details' : 'Show Details'}</Button>
+                        {expandedTaskId === task.id && (
+                            <div>    
+                                <p><strong>Due Date: </strong> {task.taskEnd ? new Date(task.taskEnd).toLocaleString() : 'N/A'}</p>
+                                <p><strong>Status: </strong> {task.complete? 'Complete!' : 'Pending'}</p>
+                            </div>
+                        )}
                         <Button variant="success" onClick={() => handleCompleteTask(task.id)}>Complete Task</Button>
                         <Button variant="danger" onClick={() => handleRemoveTask(task.id)}>Remove Task</Button>
                     </ListGroup.Item>
